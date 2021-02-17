bf57dc29fdefad7860d4c86bc062eae96b17872c
Erika Smith <erikaannesmith@gmail.com>
2021-02-17 11:29:07 -0500

    Add some content to index page; a couple styling clean up items

--- src/styles/index.styles.js
+++ src/styles/index.styles.js
@@ -1,5 +1,22 @@
 import styled from 'styled-components'
 
+export const StyledContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 export const StyledProfileImage = styled.img`
   border-radius: 50%;
+`;
+
+export const StyledInfoTitle = styled.span`
+  font-weight: 600;
+  min-width: 300px;
+`;
+
+export const StyledInfoSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  line-height: 30px;
 `;
