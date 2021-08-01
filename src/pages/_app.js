1bb605ca34f558c29f679980e99b4a3e250336db
Erika Smith <erika.smith@formidable.com>
2021-08-01 12:43:45 -0600

    Google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,26 @@ import React from "react";
 import "../styles/globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import * as ga from "../lib/ga";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <div className="page">
       <div>
